1addcc3909c14ba5c0638fb77ba107625a15aa92
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2023-05-31 13:46:16 +0100

    add opening

--- opening/models.py
+++ opening/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from authent.models import Company
+from uuid import uuid4
 
 
 # Create your models here.
 class Opening(models.Model):
+
+    uid = models.UUIDField(default=uuid4, editable=False, unique=True)
     headline = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     pay = models.CharField(max_length=50)
