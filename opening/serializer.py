4c7a97a29c3c714c24bada04b53b97c1216d9cad
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2023-05-31 11:17:47 +0100

    add opening

--- opening/serializer.py
+++ opening/serializer.py
@@ -5,5 +5,5 @@ from .models import Opening
 class OpeningSerializer(serializers.ModelSerializer):
     class Meta:
         model = Opening
-        fields = ['id', 'headline', 'description', 'pay', 'level', 'job_type', 'requirements', 'created_at', 'updated_at']
+        fields = ['id', 'headline', 'description', 'pay', 'level', 'job_type', 'requirements', 'created_at']
 
