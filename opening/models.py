87d0d40be57ff8da2396f27f0a1b19d49191c0c1
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2023-05-31 11:14:05 +0100

    add opening

--- opening/models.py
+++ opening/models.py
@@ -12,6 +12,7 @@ class Opening(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     job_type = models.CharField(max_length=50)
     requirements = ArrayField(models.CharField(max_length=250), blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.headline
