7209e3fb097ac8a38f4eb862d503eb9602205ae7
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2023-06-05 19:00:17 +0100

    apply to opening

--- opening/urls.py
+++ opening/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from opening.views import add_opening, get_my_openings, get_opening, get_openings, apply_opening
+from opening.views import add_opening, get_my_openings, get_opening, get_openings, apply_opening, get_applications, get_my_applications
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('get-opening/<str:uid>/', get_opening, name='get-opening'),
     path('get-openings/', get_openings, name='get-openings'),
     path('apply-opening/<str:uid>/', apply_opening, name='apply-opening'),
+    path('get-applications/', get_applications, name='get-applications'),
 
 ]
